438c8bfd2e1bc20340be88db293ecdbef50255f5
pw45000 <59071826+pw45000@users.noreply.github.com>
2023-05-08 15:33:51 -0400

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -25,11 +25,13 @@ def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
 
+    :name: get_random_ingredients- hi 
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
     :return: The ingredients list.
     :rtype: list[str]
     :acknowledgement: Test
+    :description: This is a function. 
     """
     return ["shells", "gorgonzola", "parsley"]
